0036bae49a449e5fcb0d82e43263249d743a2404
User <User@WIN-AHOVF1O1E8L>
2020-01-26 12:10:34 +0300

    Init git

---
+++ src/components/MathValue.hx
@@ -0,0 +1,31 @@
+class MathValue {
+	var value:Int = 0;
+
+	static function main(number:Int) {
+        value = number;
+	}
+
+	private function add(number:Int) {
+		value += number;
+	}
+
+	private function minus(number:Int) {
+		value -= number;
+	}
+
+	public function getValue():Int {
+		return value;
+	}
+
+	public function addValue(number:Int) {
+		if(number <= 10 || number > 0) {
+			add(number);
+		}
+	}
+
+	public function minusValue(number:Int) {
+		if(number <= 10 || number > 0) {
+			minus(number);
+		}
+	}
+}
